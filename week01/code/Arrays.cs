91e81dbee92b258e42eb9cbcd8b11a1109d660e7
kimkwatson <kimwatson731@gmail.com>
2024-11-18 11:26:40 -0700

    week 2 learning

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -13,7 +13,7 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        // Create an array
+        // Create an array using 'length' as the capacity size
         // Use a for loop to iterate through the array length
         // Establish an index variable in for loop for counting iterations
         // Calculate the next multiple in array by multiplying index (+1 to offset 0) by 'number'
@@ -43,5 +43,8 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        // Remove the last values from the list up to 'amount'
+        // Insert those values at the beginning of the list
     }
 }
